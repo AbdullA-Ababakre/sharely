960fb787126b0bd68f3e08d6a42a86522bf8759f
yasen002 <362yasen2@gmail.com>
2023-12-29 22:03:42 -0800

    added dreamore AI

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./index.module.scss";
 import ButtonPrimary from "../../component/Button";
 import { isArray } from "lodash";
